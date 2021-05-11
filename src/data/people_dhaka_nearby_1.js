01c8ad978ae11539f18cde3556e8f52411978b9c
polodev <polodev10@gmail.com>
2021-05-11 18:47:40 +0600

    --updated--

--- src/data/people_dhaka_nearby_1.js
+++ src/data/people_dhaka_nearby_1.js
@@ -9,6 +9,16 @@ var people_dhaka_nearby_1 = [
         location: 'Dhaka',
         friend: false,
     },
+    {
+        title: 'Faisal vai archtree',
+        url: 'https://www.facebook.com/ironmaidenpantera',
+        description: 'Faisal vai',
+        image: '',
+        star: 1,
+        tags: '',
+        location: 'Dhaka',
+        friend: false,
+    },
 ];
 
 export default people_dhaka_nearby_1;

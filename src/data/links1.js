89c48e7863f9fa1a1ac8787c3d335d11f0c097ab
polodev <polodev10@gmail.com>
2022-10-24 11:05:33 +0600

    --updated--

--- src/data/links1.js
+++ src/data/links1.js
@@ -21,28 +21,6 @@ var links1 = [
         fb_id: '',
         friend: false,
     },
-     {
-        title: 'Nita Jui (জুঁই)',
-        url: 'https://www.facebook.com/nita.sarkar.5036',
-        description: 'B.Sc in Textile Engineering at Ahsanullah University of Science & Technology',
-        image: '',
-        star: 6,
-        tags: 'Looks Tall',
-        location: 'Dhaka / Rajshahi',
-        fb_id: '',
-        friend: true,
-    },
-    {
-        title: 'bristy debnath',
-        url: 'https://www.facebook.com/bristy.debnath.568',
-        description: 'Daffodil',
-        image: '',
-        star: 7,
-        tags: 'Looks average',
-        location: 'Dhaka Kishorgonj',
-        fb_id: '',
-        friend: true,
-    },
 
 ];
 
